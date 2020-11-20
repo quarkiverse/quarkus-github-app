634bfa30e869db24fe0c52b5cca38794abf9f179
Guillaume Smet <guillaume.smet@gmail.com>
2020-11-20 19:48:19 +0100

    @ConfigFile doesn't need to be a @Qualifier

--- runtime/src/main/java/io/quarkiverse/githubapp/ConfigFile.java
+++ runtime/src/main/java/io/quarkiverse/githubapp/ConfigFile.java
@@ -6,11 +6,8 @@ import static java.lang.annotation.RetentionPolicy.RUNTIME;
 import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
 
-import javax.inject.Qualifier;
-
 @Target(PARAMETER)
 @Retention(RUNTIME)
-@Qualifier
 public @interface ConfigFile {
 
     String value();
