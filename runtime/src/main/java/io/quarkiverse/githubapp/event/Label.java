8a687729c464a89bc39bc3f50e91f8a5c04067e3
Guillaume Smet <guillaume.smet@gmail.com>
2021-04-21 21:26:22 +0200

    Add support for label event

---
+++ runtime/src/main/java/io/quarkiverse/githubapp/event/Label.java
@@ -0,0 +1,48 @@
+package io.quarkiverse.githubapp.event;
+
+import static java.lang.annotation.ElementType.PARAMETER;
+import static java.lang.annotation.ElementType.TYPE;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import javax.inject.Qualifier;
+
+import org.kohsuke.github.GHEventPayload;
+
+@Event(name = "label", payload = GHEventPayload.Label.class)
+@Target({ PARAMETER, TYPE })
+@Retention(RUNTIME)
+@Qualifier
+public @interface Label {
+
+    String value() default Actions.ALL;
+
+    @Label(Created.NAME)
+    @Target(PARAMETER)
+    @Retention(RUNTIME)
+    @Qualifier
+    public @interface Created {
+
+        String NAME = Actions.CREATED;
+    }
+
+    @Label(Deleted.NAME)
+    @Target(PARAMETER)
+    @Retention(RUNTIME)
+    @Qualifier
+    public @interface Deleted {
+
+        String NAME = Actions.DELETED;
+    }
+
+    @Label(Edited.NAME)
+    @Target(PARAMETER)
+    @Retention(RUNTIME)
+    @Qualifier
+    public @interface Edited {
+
+        String NAME = Actions.EDITED;
+    }
+}
