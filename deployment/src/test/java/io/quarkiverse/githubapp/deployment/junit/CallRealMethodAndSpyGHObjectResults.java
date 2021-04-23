bb0f4de2022dcc487ad8f73b17353947e5979a48
Guillaume Smet <guillaume.smet@gmail.com>
2021-04-23 19:31:53 +0200

    Create an ad hoc mocking infrastructure
    I had tons of issues trying to create that, probably something I need to
    discuss with Stuart and Martin.

---
+++ deployment/src/test/java/io/quarkiverse/githubapp/deployment/junit/CallRealMethodAndSpyGHObjectResults.java
@@ -0,0 +1,35 @@
+package io.quarkiverse.githubapp.deployment.junit;
+
+import static org.mockito.Mockito.withSettings;
+
+import java.io.Serializable;
+
+import org.kohsuke.github.GHObject;
+import org.mockito.Mockito;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
+
+final class CallRealMethodAndSpyGHObjectResults implements Answer<Object>, Serializable {
+
+    private final GitHubMockContextImpl mocks;
+
+    public CallRealMethodAndSpyGHObjectResults(GitHubMockContextImpl mocks) {
+        this.mocks = mocks;
+    }
+
+    @Override
+    public Object answer(InvocationOnMock invocation) throws Throwable {
+        Object original = invocation.callRealMethod();
+        if (!(original instanceof GHObject)) {
+            return original;
+        }
+        GHObject castOriginal = (GHObject) original;
+        Class<? extends GHObject> type = castOriginal.getClass();
+        DefaultableMocking<? extends GHObject> mocking = mocks.ghObjectMocking(castOriginal);
+        return Mockito.mock(type, withSettings().stubOnly()
+                .withoutAnnotations()
+                .spiedInstance(original)
+                .defaultAnswer(new CallMockedMethodOrCallRealMethodAndSpyGHObjectResults(this, mocking)));
+    }
+
+}
