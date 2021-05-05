1f0f37b939314e2840e0a3f6cabdc63f9a590e67
Yoann Rodière <yoann@hibernate.org>
2021-05-05 08:49:36 +0200

    Support for using multiple GitHubAppTesting.given() / GitHubAppTesting.when() in the same test method

--- testing/src/main/java/io/quarkiverse/githubapp/testing/internal/GitHubAppTestingCallback.java
+++ testing/src/main/java/io/quarkiverse/githubapp/testing/internal/GitHubAppTestingCallback.java
@@ -6,13 +6,9 @@ import io.quarkiverse.githubapp.runtime.github.GitHubFileDownloader;
 import io.quarkiverse.githubapp.runtime.github.GitHubService;
 import io.quarkus.test.junit.QuarkusMock;
 import io.quarkus.test.junit.callback.QuarkusTestAfterConstructCallback;
-import io.quarkus.test.junit.callback.QuarkusTestAfterEachCallback;
-import io.quarkus.test.junit.callback.QuarkusTestBeforeEachCallback;
-import io.quarkus.test.junit.callback.QuarkusTestMethodContext;
 
 public final class GitHubAppTestingCallback
-        implements QuarkusTestAfterConstructCallback,
-        QuarkusTestBeforeEachCallback, QuarkusTestAfterEachCallback {
+        implements QuarkusTestAfterConstructCallback {
 
     private static final String ENABLED_KEY = "quarkiverse-github-app-testing.enabled";
 
@@ -34,21 +30,5 @@ public final class GitHubAppTestingCallback
         GitHubMockContextImpl mocks = GitHubAppTestingContext.get().mocks;
         QuarkusMock.installMockForType(mocks.service, GitHubService.class);
         QuarkusMock.installMockForType(mocks.fileDownloader, GitHubFileDownloader.class);
-    }
-
-    @Override
-    public void beforeEach(QuarkusTestMethodContext context) {
-        if (!isEnabled()) {
-            return;
-        }
-        GitHubAppTestingContext.get().mocks.init();
-    }
-
-    @Override
-    public void afterEach(QuarkusTestMethodContext context) {
-        if (!isEnabled()) {
-            return;
-        }
-        GitHubAppTestingContext.get().mocks.reset();
     }
 }
