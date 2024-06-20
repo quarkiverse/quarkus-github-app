fc0a7ccaa22aafe967b4f88b9ac77dc1a15874e5
melloware <mellowaredev@gmail.com>
2024-06-20 09:42:45 -0400

    Fix #627: Dev UI Card

---
+++ deployment/src/main/java/io/quarkiverse/githubapp/deployment/devui/GitHubAppDevUIProcessor.java
@@ -0,0 +1,33 @@
+package io.quarkiverse.githubapp.deployment.devui;
+
+import io.quarkus.deployment.IsDevelopment;
+import io.quarkus.deployment.annotations.BuildProducer;
+import io.quarkus.deployment.annotations.BuildStep;
+import io.quarkus.devui.spi.page.CardPageBuildItem;
+import io.quarkus.devui.spi.page.ExternalPageBuilder;
+import io.quarkus.devui.spi.page.Page;
+import io.quarkus.vertx.http.deployment.NonApplicationRootPathBuildItem;
+
+/**
+ * Dev UI card for displaying important details such as the GitHub App Replay UI.
+ */
+public class GitHubAppDevUIProcessor {
+
+    @BuildStep(onlyIf = IsDevelopment.class)
+    void createDevCard(BuildProducer<CardPageBuildItem> cardPageBuildItemBuildProducer,
+            NonApplicationRootPathBuildItem nonApplicationRootPathBuildItem) {
+        final CardPageBuildItem card = new CardPageBuildItem();
+
+        final String uiPath = nonApplicationRootPathBuildItem.resolvePath("/replay/");
+
+        final ExternalPageBuilder versionPage = Page.externalPageBuilder("Replay UI")
+                .icon("font-awesome-solid:play")
+                .url(uiPath, uiPath)
+                .doNotEmbed();
+        card.addPage(versionPage);
+
+        card.setCustomCard("qwc-github-app-card.js");
+
+        cardPageBuildItemBuildProducer.produce(card);
+    }
+}
